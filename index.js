68dbc35d8773ea964596e5d2d4bebcfa6f7bbf3e
Suei43 <chrisraphael576@gmail.com>
2023-08-07 11:51:07 +0100

    Feat: folders containing images

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ require('dotenv').config()
 const imageRouter = require('./src/routes/imageRoute');
 
 app.use('/api', imageRouter);
-app.use('/uploads', express.static('uploads'));
+app.use('/public', express.static('public'));
 
 app.get('/api',(req, res)=>{
-    res.send("Welcome to image classification API")
+    res.json({"message":"Welcome to image classification API",
+                "type": "success",
+                "documentation_url": "https://github.com/Suei43/imagevision",
+                "author": "Suei",
+                "note": "Make a POST request to /api/image-classification to classify an image. You can use the confidenceThreshold query parameter to specify the confidence threshold for the classification. The default value is 0.1. You can also upload multiple images at once by using the multiple attribute in the file input field."})
 })
 
 app.listen(process.env.PORT || 3000, ()=>{
-    console.log(`Server is running on port ${process.env.PORT}.`)
+    console.log(`Server is running on port https://localhost:${process.env.PORT || 3000}`)
 })
 
 module.exports = app;
--- src/routes/imageRoute.js
+++ src/routes/imageRoute.js
@@ -1,40 +1,69 @@
 const express = require('express');
 const multer = require('multer');
 const sharp = require('sharp');
-const detectObject = require('../detection');
+const detectObject = require('../utils/detection');
 
 const router = express.Router();
-let upload = multer({ dest: 'uploads/' })
+let upload = multer({ dest: 'public/' });
 
-router.post('/image-recognition', upload.single('image'), async (req, res) => {
-    if (!req.file)
-      return res.status(400).json({ error: 'Please upload an image' });
+router.post('/image-classification', upload.single('image'), async (req, res) => {
+  if (!req.file && !req.files)
+    return res.status(400).json({ error: 'Please upload an image.' });
 
-    //If user specifies a confidence threshold, check if it is between 0 and 1
-    if(req.body.confidenceThreshold){
-      if(req.body.confidenceThreshold < 0 || req.body.confidenceThreshold > 1){
-        return res.status(400).json({ error: 'Confidence threshold must be between 0 and 1' });
-      }
+  // If user specifies a confidence threshold, check if it is between 0 and 1
+  if (req.body.confidenceThreshold) {
+    if (req.body.confidenceThreshold < 0 || req.body.confidenceThreshold > 1) {
+      return res
+        .status(400)
+        .json({ error: 'Confidence threshold must be between 0 and 1' });
     }
-  
-    const processedImageBuffer = await sharp(req.file.path)
+  }
+
+  try {
+    if (req.file) {
+      // If a single image is uploaded, process and detect objects in it
+      const processedImageBuffer = await processImage(req.file.path);
+      const imageClassification = await detectObject(
+        processedImageBuffer,
+        req.body.confidenceThreshold || 0.1
+      );
+      res.status(200).json({
+        message: 'Image classification successful',
+        fileName: req.file.originalname,
+        classification: imageClassification,
+      });
+    } else if (req.files && req.files.length > 0) {
+      // If multiple images are uploaded, process and detect objects in each of them
+      const results = [];
+      for (const file of req.files) {
+        const processedImageBuffer = await processImage(file.path);
+        const imageClassification = await detectObject(
+          processedImageBuffer,
+          req.body.confidenceThreshold || 0.1
+        );
+        results.push({
+          id: file.filename,
+          fileName: file.originalname,
+          classification: imageClassification,
+        });
+      }
+      res.status(200).json({
+        message: 'Image classification successful',
+        classifications: results,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err, message: 'Image classification failed' });
+  }
+});
+
+async function processImage(imagePath) {
+  let processedImageBuffer = await sharp(imagePath)
     .resize(224, 224)
     .toFormat('jpeg')
     .toBuffer();
-
-    detectObject(processedImageBuffer, confidenceThreshold = req.body.confidenceThreshold || 0.1)
-    .then((imageClassification) => {
-      res.status(200).json({
-        message: 'Image classification successful',
-        classification: imageClassification,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res
-        .status(500)
-        .json({ error: err, message: 'Image classification failed' });
-    });
-});
+  return processedImageBuffer;
+}
 
 module.exports = router;
